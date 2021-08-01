54456e092d7e707b992610989edf5ab88c23d082
Jack Kinga <jackmwangi@gmail.com>
2021-08-01 20:12:57 +0300

    add for loop support

--- src/main/java/ke/co/tookie/Parser.java
+++ src/main/java/ke/co/tookie/Parser.java
@@ -1,6 +1,7 @@
 package ke.co.tookie;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static ke.co.tookie.TokenType.*;
@@ -13,10 +14,14 @@ import static ke.co.tookie.TokenType.*;
 // varDecl       → "var" IDENTIFIER ( "=" expression )? ";" ;
 //
 // statement     → exprStmt
+//               | forStmt
 //               | ifStmt
 //               | printStmt
 //               | whileStmt
 //               | block;
+// forStmt       → "for" "(" ( varDecl | exprStmt | ";" )
+//                 expression? ";"
+//                 expression? ")" statement ;
 //
 // whileStmt     → "while" "(" expression ")" statement ;
 //
@@ -69,12 +74,55 @@ class Parser {
   }
 
   private Stmt statement() {
+    if (match(FOR)) return forStatement();
     if (match(IF)) return ifStatement();
     if (match(PRINT)) return printStatement();
     if (match(WHILE)) return whileStatement();
     if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
     return expressionStatement();
+  }
+
+  private Stmt forStatement() {
+    consume(LEFT_PAREN, "Expect '(' after 'for'.");
+
+    Stmt initializer;
+    if (match(SEMICOLON)) {
+      initializer = null;
+    } else if (match(VAR)) {
+      initializer = varDeclaration();
+    } else {
+      initializer = expressionStatement();
+    }
+
+    Expr condition = null;
+    if (!check(SEMICOLON)) {
+      condition = expression();
+    }
+    consume(SEMICOLON, "Expect ';' after loop condition");
+
+    Expr increment = null;
+    if (!check(RIGHT_PAREN)) {
+      increment = expression();
+    }
+    consume(RIGHT_PAREN, "Expect ')' after for clause");
+    Stmt body = statement();
+
+    if (increment != null) {
+      body = new Stmt.Block(
+        Arrays.asList(
+          body,
+          new Stmt.Expression(increment)));
+    }
+
+    if (condition == null) condition = new Expr.Literal(true);
+    body = new Stmt.While(condition, body);
+
+    if (initializer != null) {
+      body = new Stmt.Block(Arrays.asList(initializer, body));
+    }
+
+    return body;
   }
 
   private Stmt ifStatement() {
@@ -138,7 +186,7 @@ class Parser {
   private List<Stmt> block() {
     List<Stmt> statements = new ArrayList<>();
 
-    while(!check(RIGHT_BRACE) && !isAtEnd()) {
+    while (!check(RIGHT_BRACE) && !isAtEnd()) {
       statements.add(declaration());
     }
 
@@ -147,7 +195,7 @@ class Parser {
   }
 
   private Expr assignment() {
-    Expr expr = or();
+    Expr expr = or ();
 
     if (match(EQUAL)) {
       Token equals = previous();
@@ -164,24 +212,24 @@ class Parser {
     return expr;
   }
 
-  private Expr or() {
-    Expr left = and();
+  private Expr or () {
+    Expr left = and ();
 
-    while(match(OR)) {
+    while (match(OR)) {
       Token operator = previous();
-      Expr right = and();
+      Expr right = and ();
       left = new Expr.Logical(left, operator, right);
     }
 
     return left;
   }
 
-  private Expr and() {
+  private Expr and () {
     Expr left = equality();
 
-    while(match(AND)) {
+    while (match(AND)) {
       Token operator = previous();
-      Expr right = and();
+      Expr right = and ();
       left = new Expr.Logical(left, operator, right);
     }
 
