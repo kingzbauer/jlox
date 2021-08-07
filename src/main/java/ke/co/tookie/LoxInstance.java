8d83e489ee6d277bed91ff95ce48815636c744fb
Jack Kinga <jackmwangi@gmail.com>
2021-08-07 11:30:05 +0300

    create class instances

---
+++ src/main/java/ke/co/tookie/LoxInstance.java
@@ -0,0 +1,14 @@
+package ke.co.tookie;
+
+class LoxInstance {
+  private LoxClass klass;
+
+  LoxInstance(LoxClass klass) {
+    this.klass = klass;
+  }
+
+  @Override
+  public String toString() {
+    return klass.name + " instance";
+  }
+}
